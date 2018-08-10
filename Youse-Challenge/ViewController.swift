5b61afd79970d2042fe60a17841179a97a6dada2
Michael Douglas <michael.dcsouza@gmail.com>
2018-08-10 17:59:13 -0300

    - Created Base Project

---
+++ Youse-Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Youse-Challenge
+//
+//  Created by Michael Douglas on 10/08/18.
+//  Copyright © 2018 Michael Douglas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

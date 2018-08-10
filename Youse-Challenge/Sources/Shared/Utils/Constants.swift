ae897fab0a7979f4b7f3c89054742a64720af041
Michael Douglas <michael.dcsouza@gmail.com>
2018-08-10 18:11:47 -0300

    - Created Base Project Structure,  - Add Localizable Files,  - Add Constants File

---
+++ Youse-Challenge/Sources/Shared/Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Youse-Challenge
+//
+//  Created by Michael Douglas on 10/08/18.
+//  Copyright © 2018 Michael Douglas. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    
+}

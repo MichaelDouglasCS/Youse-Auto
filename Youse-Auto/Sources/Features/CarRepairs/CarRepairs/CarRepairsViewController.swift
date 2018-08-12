7a6d4638339ea46b14e7a87afa4a255e469eafe3
Michael Douglas <michael.dcsouza@gmail.com>
2018-08-12 16:48:40 -0300

    - Add CarRepairs

---
+++ Youse-Auto/Sources/Features/CarRepairs/CarRepairs/CarRepairsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  CarRepairsViewController.swift
+//  Youse-Auto
+//
+//  Created by Michael Douglas on 12/08/18.
+//  Copyright © 2018 Michael Douglas. All rights reserved.
+//
+
+import UIKit
+
+class CarRepairsViewController: UIViewController {
+    
+    //*************************************************
+    // MARK: - Outlets
+    //*************************************************
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    //*************************************************
+    // MARK: - Lifecycle
+    //*************************************************
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Youse-Auto/Sources/Features/CarRepairs/CarRepairs/CarRepairsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CarRepairsViewModel.swift
+//  Youse-Auto
+//
+//  Created by Michael Douglas on 12/08/18.
+//  Copyright © 2018 Michael Douglas. All rights reserved.
+//
+
+import Foundation
+
+class CarRepairsViewModel: NSObject {
+    
+}
